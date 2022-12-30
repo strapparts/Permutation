0bde0032794c892ed771a189d4708a9565cc4bb3
strapparts <a.strappa61@gmail.com>
2022-12-30 22:08:38 +0100

    aggiunte le quattro formule (che compaiono in una textView quando si preme su "invio"), e due didascalie ("permutazione" e "combinazione").

--- app/src/main/java/com/example/permutations/MainActivity.java
+++ app/src/main/java/com/example/permutations/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity{
 
     private EditText inputTextNumber, inputTextNumber2;
-    private TextView textViewResult;
+    private TextView textViewResult, textViewFormula, perm, comb;
     public RadioGroup radioGroupRep, radioGroupOrder;
     public Button enterButton;
     PermutationsNumber permNum = new PermutationsNumber(1, 1);
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity{
         //https://stackoverflow.com/questions/6217378/place-cursor-at-the-end-of-text-in-edittext
         inputTextNumber2.setSelection(inputTextNumber.getText().length());
         textViewResult = findViewById(R.id.textview_result);
+        textViewFormula = findViewById(R.id.textView4);
+        perm = findViewById(R.id.textView5);
+        comb = findViewById(R.id.textView6);
         enterButton = findViewById(R.id.button_result);
         RadioButton radioWithoutRep = findViewById(R.id.radio_without_rep);
         RadioButton radioWithRep = findViewById(R.id.radio_with_rep);
@@ -42,18 +45,26 @@ public class MainActivity extends AppCompatActivity{
 
             if (radioWithoutRep.isChecked() && radioWithOrder.isChecked()){
                 if (num1 < num2) {
-                    textViewResult.setText("n < k !!!");} else {
+                    textViewResult.setText("n < k !!!");
+                    textViewFormula.setText("n!/(n-k)!");}
+                else {
                     textViewResult.setText(String.valueOf(permNum.getWithoutRep()));
+                    textViewFormula.setText("n!/(n-k)!");
                 }
             } else if (radioWithRep.isChecked() && radioWithOrder.isChecked()) {
                 textViewResult.setText(String.valueOf(permNum.getWithRep()));
+                textViewFormula.setText("n^k");
             } else if (radioWithoutRep.isChecked() && radioWithoutOrder.isChecked()){
                 if (num1 < num2) {
-                    textViewResult.setText("n < k !!!");} else {
+                    textViewResult.setText("n < k !!!");
+                    textViewFormula.setText("(n+k-1)!/k!(n-1)!");}
+                else {
                     textViewResult.setText(String.valueOf(permNum.getWithoutRepWithoutOrder()));
+                    textViewFormula.setText("n!/k!(n-k)!");
                 }
             } else if (radioWithRep.isChecked() && radioWithoutOrder.isChecked()){
                 textViewResult.setText(String.valueOf(permNum.getWithRepWithoutOrder()));
+                textViewFormula.setText("(n+k-1)!/k!(n-1)!");
             }
         });
     }
